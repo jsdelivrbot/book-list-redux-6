5fcbd18b820264495563d911d966afd455fa2e30
Carolina Vasquez-Ceja <cejac10@gmail.com>
2017-05-09 22:54:29 -0700

    creating an action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,8 @@
 export function selectBook(book) {
-  console.log('A book has been seleced:' book.title)
+  // selectBook is an Action Creator, it nees to return an actions
+  // an object with a type property
+  return {
+    type: 'BOOK_SELECTED', //Action Creator always contains type. Uppercase, usually a string
+    payload: book //usually has a payload
+  };
 }
