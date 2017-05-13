ed016d5c1a56d5f5c3e216ad0c916865349ce527
Carolina Vasquez-Ceja <cejac10@gmail.com>
2017-05-13 15:50:47 -0700

    conditional rendering

--- src/reducers/reducer-books.js
+++ src/reducers/reducer-books.js
@@ -1,8 +1,8 @@
 export default function() {
   return[
-    {title: 'JavaScript: The Good Parts'},
-    {title: 'Harry Potter and the Order of the Pheonix'},
-    {title: 'The Dark Tower'},
-    {title: 'The Way of Kings'}
+    {title: 'JavaScript: The Good Parts', pages: 10},
+    {title: 'Harry Potter and the Order of the Pheonix', pages: 500},
+    {title: 'The Dark Tower', pages: 454},
+    {title: 'The Way of Kings', pages: 1000}
   ]
 }
